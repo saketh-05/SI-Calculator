ecc9565a632776dde8d86df081006e265b8c8aa2
Saketh05 <raisingsurya05@gmail.com>
2023-05-25 15:59:01 +0530

    New Website

---
+++ script.js
@@ -0,0 +1,14 @@
+function function1(){
+    var input= document.getElementById("userinput1").value;
+
+    var input1= document.getElementById("userinput2").value;  
+
+    var input2= document.getElementById("userinput3").value;
+
+
+var  result=(input * input1 * input2) / 100;
+
+var resultExp=document.getElementById("output");
+
+resultExp.innerHTML= result;
+}
