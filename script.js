79a808e5cf9bef949bc3a1ac86e1a2ec5b1ced59
Saketh05 <raisingsurya05@gmail.com>
2023-05-25 19:05:19 +0530

    Changes

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
 function function1(){
     var input= document.getElementById("userinput1").value;
-
     var input1= document.getElementById("userinput2").value;  
-
     var input2= document.getElementById("userinput3").value;
 
-
 var  result=(input * input1 * input2) / 100;
-
 var resultExp=document.getElementById("output");
-
 resultExp.innerHTML= result;
 }
 
 function getAboutDetails(){
-
-var aboutdetails=document.querySelector(".about-details");
-
+var aboutdetails=document.querySelector("about-details");
 aboutdetails.style.display="block";
 }
