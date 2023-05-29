8b84ebb3846d0334c651f831f7e1a54d4e2dfab1
Saketh05 <raisingsurya05@gmail.com>
2023-05-29 15:33:21 +0530

    Additional changes

--- script.js
+++ script.js
@@ -10,9 +10,24 @@ resultExp.innerHTML= result;
 
 document.getElementById("firstbutton").addEventListener("click",function(){
    window.location.href="newpage.html";
-})
+});
 
 
 document.getElementById("homebutton").addEventListener("click",function(){
     window.location.href="index.html";
-})
+});
+
+function function2(){
+    var inp=document.getElementById("input1").value;
+    var inp1=document.getElementById("input2").value;
+    var inp2=document.getElementById("input3").value;
+    var inp3=document.getElementById("input4").value;
+
+   var output= inp * [ ( 1 + ( inp2 / ( 100 * inp3 ) ) ) ** ( inp3 * inp1 ) - 1 ];
+   var outputexp=document.getElementById("output");
+   outputexp.innerHTML= Math.round(output);
+
+   var sum = Number(inp) + output ;
+   var finalres=document.getElementById("finalresult");
+   finalres.innerHTML=Math.round(sum);
+}
